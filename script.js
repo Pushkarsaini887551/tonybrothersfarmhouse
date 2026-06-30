0524edef3e41a3b53ec15129bfc8c4fd38c14a4c
Pushkarsaini887551 <PUSHKARSAINI358@GMAIL.COM>
2026-06-30 17:21:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,40 @@
-fetch("database.json")
+let trees = [];
+
+fetch("trees.json")
 .then(response => response.json())
 .then(data => {
-
-window.searchTree = function(){
-
-let tree = document.getElementById("searchInput").value.trim().toLowerCase();
-
-let result = document.getElementById("result");
-
-let found = data.find(item =>
-item.name.toLowerCase() === tree ||
-item.english.toLowerCase() === tree
-);
-
-if(found){
-
-result.innerHTML = `
-<div class="card">
-<h2>${found.name}</h2>
-<p><b>English:</b> ${found.english}</p>
-<p><b>Scientific:</b> ${found.scientific}</p>
-<p>${found.info}</p>
-</div>
-`;
-
-}else{
-
-result.innerHTML = "<h2>❌ पेड़ नहीं मिला</h2>";
-
-}
-
-}
-
+    trees = data;
 });
+
+function searchTree() {
+
+    let text = document.getElementById("searchInput").value.trim().toLowerCase();
+
+    let found = trees.find(tree =>
+        tree.name.toLowerCase() === text ||
+        tree.english.toLowerCase() === text
+    );
+
+    let result = document.getElementById("result");
+
+    if(found){
+
+        result.innerHTML = `
+        <div class="card">
+            <h2>${found.name}</h2>
+            <p><b>English:</b> ${found.english}</p>
+            <p>${found.info}</p>
+        </div>
+        `;
+
+    }else{
+
+        result.innerHTML = `
+        <div class="card">
+            <h2>❌ पेड़ नहीं मिला</h2>
+        </div>
+        `;
+
+    }
+
+}
