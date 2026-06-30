c865d979176a0df933c7c020b9a8daeff305cfd9
Pushkarsaini887551 <PUSHKARSAINI358@GMAIL.COM>
2026-06-30 17:07:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-fetch("trees.json")
+fetch("database.json")
 .then(response => response.json())
 .then(data => {
 
-window.searchTree = function() {
+window.searchTree = function(){
 
 let tree = document.getElementById("searchInput").value.trim().toLowerCase();
 
@@ -17,8 +17,9 @@ if(found){
 
 result.innerHTML = `
 <div class="card">
-<img src="${found.image}" style="width:100%;border-radius:10px;">
 <h2>${found.name}</h2>
+<p><b>English:</b> ${found.english}</p>
+<p><b>Scientific:</b> ${found.scientific}</p>
 <p>${found.info}</p>
 </div>
 `;
